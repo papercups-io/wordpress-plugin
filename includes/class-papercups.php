362d0a3943460c0aa630c44c372c14e7cb6b098d
zmagoD <zmago_devetak@yahoo.com>
2020-08-25 23:12:56 +0200

    remove unused hook

--- includes/class-papercups.php
+++ includes/class-papercups.php
@@ -153,12 +153,9 @@ class Papercups
 	 */
 	private function define_admin_hooks()
 	{
-
 		$plugin_admin = new Papercups_Admin($this->get_plugin_name(), $this->get_version());
-
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
 		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
-		$this->loader->add_action('admin_menu', $plugin_admin, 'add_plugin_admin_menu');
 		$this->loader->add_action('admin_init', $plugin_admin, 'init_settings');
 	}
 
